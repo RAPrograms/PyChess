baaf38a6f0e876b2934bdb21bf79de62acdda9a8
Dog2657 <RAPrograms@outlook.com>
2025-08-29 23:06:05 +0100

    Bishop movement working

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -1,6 +1,37 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from dataclasses.enums import Direction, Team
+from dataclasses.units import Coordinate
 from .piece import Piece
-from dataclasses.enums import Team
+
+if TYPE_CHECKING:
+    from mvc.model import Model
 
 class Bishop(Piece):
     def __init__(self, team: Team):
         super().__init__(team, "Bishop")
+
+    def get_valid_movements(self,
+        position: Coordinate,
+        model: Model,
+    ) -> list[Coordinate]:
+        output = []
+        for dir in [
+            Direction.UpLeft,
+            Direction.UpRight,
+            Direction.DownLeft,
+            Direction.DownRight
+        ]:
+            for pos, _ in position.itterate_direction(dir):
+                content = model.get_cell(pos)
+                if(content is not None and content.team == self.team):
+                    break
+
+                output.append(pos)
+                if(content is not None):
+                    break
+
+                continue
+
+        return output
