881421c627e6b6aff3226e546a44a0824b2cb1bc
Dog2657 <RAPrograms@outlook.com>
2025-08-29 23:32:10 +0100

    Implimented king movement

--- pieces/king.py
+++ pieces/king.py
@@ -1,6 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from dataclasses.enums import Direction, Team
+from dataclasses.units import Coordinate
 from .piece import Piece
-from dataclasses.enums import Team
+
+if TYPE_CHECKING:
+    from mvc.model import Model
 
 class King(Piece):
     def __init__(self, team: Team):
         super().__init__(team, "King")
+
+    def get_valid_movements(self,
+        position: Coordinate,
+        model: Model,
+    ) -> list[Coordinate]:
+        return self.create_direction_paths(position, model, [
+            Direction.UpLeft,
+            Direction.UpRight,
+            Direction.DownLeft,
+            Direction.DownRight,
+            Direction.Up,
+            Direction.Right,
+            Direction.Down,
+            Direction.Left
+        ])
